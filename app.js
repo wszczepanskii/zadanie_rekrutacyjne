0e1156d5a3371c50cc37323a730935ec85099e8b
bvmbo <wiktorsz2001@wp.pl>
2024-11-13 20:30:43 +0100

    added function that reads text file with article

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ import fetch from 'node-fetch';
 import 'dotenv/config';
 
 const OPENAI_API_KEY = process.env.API_KEY;
+
+function readArticleFile(filePath) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(filePath, 'utf8', (err, data) => {
+			if (err) reject(err);
+			else resolve(data);
+		});
+	});
+}
